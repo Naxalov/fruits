ab35c99c97270a6923de35bb7dca5e4642b35c70
ZARIFMAC <uzbzarif@gmail.com>
2022-11-14 10:19:20 +0500

    get expensive

--- get_expensive_fruit.py
+++ get_expensive_fruit.py
@@ -8,5 +8,26 @@ def get_expensive_fruit(data:str)->str:
         name of the most expensive fruit
     """
     # your code here
-    pass
+    rows = data.split()[1:]
+    price = []
+    name = []
+
+    for row in rows:
+        price.append(float(row.split(',')[1]))
+        name.append(row.split(',')[0])
+
+
+    idx = 0
+    mprice = price[idx]
+
+    for i in range(len(price)):
+        if price[i]>mprice:
+            mprice=price[i]
+            idx = i  
+
+    return name[idx]    
+
+data = open('fruits.csv').read()
+
+print(get_expensive_fruit(data))
 
